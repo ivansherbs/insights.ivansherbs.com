94fcf41ac254b783e38d6cc1736ceec88667e265
Gabriel Petrovay <gabipetrovay@gmail.com>
2019-08-19 15:27:37 +0200

    Fixed cronjob for regular updated

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -11,9 +11,9 @@ echo "[`date`] Checking for new versions for repository: ${repo_name} (${PWD})"
 
 git fetch
 
-readonly commits_behind=`git rev-list HEAD..origin/master --count */`
+readonly content_commits_behind=`git rev-list HEAD..origin/master --count */`
 
-if [ ${commits_behind} -gt 0 ]
+if [ ${content_commits_behind} -gt 0 ]
 then
   # report the detected changes
   echo "[`date`] Removing the files that need an update from target directory: ${target_dir}"
@@ -21,11 +21,18 @@ then
 
   # remove the files that will be changed
   git rev-list HEAD..origin/master | xargs git show --pretty="" --name-only | uniq | sed 's/\.md/.html/' | xargs -I "{}" rm -f "${target_dir}/{}"
+else
+  echo "[`date`] No content updates found in the content source files"
+fi
 
+readonly repo_commits_behind=`git rev-list HEAD..origin/master --count`
+
+if [ ${repo_commits_behind} -gt 0 ]
+then
   # pull the updates for the source content files
   git pull
 else
-  echo "[`date`] No updates found in the content source files"
+  echo "[`date`] No other updates found in the content repo"
 fi
 
 echo "[`date`] Finished checking for new versions for repository: ${repo_name} (${PWD})"
