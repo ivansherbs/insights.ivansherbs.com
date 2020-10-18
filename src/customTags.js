2a9e1fdbdaefd5b59716c2bbe9d95d0aaf1217db
Gabriel Petrovay <gabriel.petrovay@swisscom.com>
2020-10-18 12:14:59 +0200

    Removed console log

--- src/customTags.js
+++ src/customTags.js
@@ -4,7 +4,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLiquidTag('resourceDomain', function() {
         return {
             render: function() {
-                console.log(process.env.RESOURCE_BASE_URL);
                 return Promise.resolve(process.env.RESOURCE_BASE_URL);
             }
         };
