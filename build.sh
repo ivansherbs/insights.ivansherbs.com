0075f926463076b32bef8dcbffcbe1713ff127d2
Gabriel Petrovay <gabipetrovay@gmail.com>
2021-01-09 22:35:08 +0100

    Correct pre-generation output directory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-npx eleventy --input content/nl/ivans/veelgestelde-vragen
+npx eleventy --input content/nl/ivans/veelgestelde-vragen --output _site/nl/ivans/veelgestelde-vragen
 npx eleventy
