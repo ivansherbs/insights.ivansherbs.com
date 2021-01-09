2691b6447c3c7244b3a389ea57306767e862c947
Gabriel Petrovay <gabipetrovay@gmail.com>
2021-01-09 22:44:10 +0100

    Added missing pre-rendered templates

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# templates pre-rendering
 npx eleventy --input content/nl/ivans/veelgestelde-vragen --output _site/nl/ivans/veelgestelde-vragen
+npx eleventy --input content/nl/ivans/voordelen --output _site/nl/ivans/voordelen
+
+# templates rendering
 npx eleventy
