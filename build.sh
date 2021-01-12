772a85024d47bb2e138bde466661f3de75198fa3
Gabriel Petrovay <gabipetrovay@gmail.com>
2021-01-12 12:34:54 +0100

    troubleshooting Netlify _redirects generation

--- build.sh
+++ build.sh
@@ -3,29 +3,44 @@
 function generate_redirects {
   echo '# Content custom URLs' > _site/_redirects
 
-  which sed >> _site/_redirects
-  grep -r 'url:' content >> _site/_redirects
+  grep -rI 'url:' content | sed 's/^/# /'>> _site/_redirects
 
-  grep --recursive --line-number --include '*.md' --regexp '^url:' content | while read -r redirect_config
+  grep --recursive --line-number --include '*.md' --binary-file=without-match --regexp '^url:' content | while read -r redirect_config
   do
-    redirect_from=$(echo "${redirect_config}" | cut -d ':' -f 1 | sed 's/^content//' | sed 's/\.md$//' )
+    redirect_from=$(echo "${redirect_config}" | cut -d ':' -f 1 | sed 's/^content//' | sed 's/\.md$//')
     redirect_to=$(echo "${redirect_config}" | cut -d ':' -f 4 | sed 's/^ *//')
 
-    echo "${redirect_from} ${redirect_to}" 200 >> _site/_redirects
+    echo "${redirect_from} ${redirect_to} 200" >> _site/_redirects
   done
 }
 
 function main {
   # templates pre-rendering
+  echo "IVAN: Pre-processing template fragments ..."
   npx eleventy --input content/nl/ivans/veelgestelde-vragen --output _site/nl/ivans/veelgestelde-vragen
   npx eleventy --input content/nl/ivans/voordelen --output _site/nl/ivans/voordelen
 
   # templates rendering
+  echo "IVAN: Processing templates ..."
   npx eleventy
 
-  # generate redirects
-  generate_redirects
+  echo '1================ Simple grep:'
+  grep -rI 'url:' content
+  echo '2================ Complete grep:'
+  grep --recursive --line-number --include '*.md' --binary-file=without-match --regexp '^url:' content
+  echo '3================ Iterate over grep results'
+  grep --recursive --line-number --include '*.md' --binary-file=without-match --regexp '^url:' content | while read -r redirect_config
+  do
+    redirect_from=$(echo "${redirect_config}" | cut -d ':' -f 1 | sed 's/^content//' | sed 's/\.md$//')
+    redirect_to=$(echo "${redirect_config}" | cut -d ':' -f 4 | sed 's/^ *//')
 
+    echo "${redirect_from} ${redirect_to} 200"
+  done
+  echo '4================'
+
+  # generate redirects
+  echo "IVAN: Generating redirects ..."
+  generate_redirects
 }
 
 main
