b2f8fbc147f26d4f34ff9f9178a5eedc8d180e2e
Gabriel Petrovay <gabriel.petrovay@swisscom.com>
2021-05-14 15:19:39 +0200

    Preparing test framework for content validation

---
+++ test/test_front_matter.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const fs = require('fs');
+const yaml = require('yaml');
+const yamllint = require('yaml-lint');
+
+const CONTENT_PATH = 'content/';
+const LANGUAGES = ['en', 'nl']
+
+describe('content front matter', function () {
+
+});
