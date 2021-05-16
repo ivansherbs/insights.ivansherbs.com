ac2ace395425bc7636c066692a8c8801d1d0f8f4
Gabriel Petrovay <gabipetrovay@gmail.com>
2021-05-16 03:14:27 +0200

    Added test for content page fragment existence

--- test/test_front_matter.js
+++ test/test_front_matter.js
@@ -9,8 +9,8 @@ const yamllint = require('yaml-lint');
 const CONTENT_PATH = 'content/';
 const LANGUAGES = ['en', 'nl'];
 
-function readFrontMatter(path, callback) {
-    const rl = readline.createInterface(fs.createReadStream(path));
+function readFrontMatter(filePath, callback) {
+    const rl = readline.createInterface(fs.createReadStream(filePath));
     var frontMatter = '';
     var readingFrontMatter = false;
     rl.on('line', (line) => {
@@ -35,7 +35,7 @@ function readFrontMatter(path, callback) {
 
 }
 
-function validateFrontMatterSyntax(filePath, callback) {
+function filterOutFilesWithValidFrontMatterSyntax(filePath, callback) {
     readFrontMatter(filePath, (err, frontMatter) => {
         if (!frontMatter) {
             callback(null, false);
@@ -47,34 +47,97 @@ function validateFrontMatterSyntax(filePath, callback) {
     });
 }
 
-describe('content front matter', function () {
+function findProblemsWithFragments(filePath, callback) {
+    readFrontMatter(filePath, (err, frontMatter) => {
+        // no front matter
+        if (!frontMatter) {
+            callback(null);
+            return;
+        }
 
-    LANGUAGES.forEach(language => {
+        var frontMatterObject = yaml.parse(frontMatter);
 
-        describe(`YAML syntax (${language})`, function () {
+        // no fragments declared
+        if (!frontMatterObject.fragments) {
+            callback(null);
+            return;
+        }
 
-            var mdFiles = [];
+        var errors = [];
 
-            before(function (done) {
-                var globOptions = {
-                    cwd: CONTENT_PATH + language
-                };
-                mdFiles = glob.sync('**/*.md', globOptions);
-                done();
-            });
+        // header fragment defined
+        if (frontMatterObject.fragments.header) {
+            var headerPath = frontMatterObject.fragments.header + '.md';
+            try {
+                fs.statSync(CONTENT_PATH + headerPath);
+            } catch (error) {
+                errors.push('header');
+            }
+        }
+
+        // footer fragment defined
+        if (frontMatterObject.fragments.footer) {
+            var footerPath = frontMatterObject.fragments.footer + '.md';
+            try {
+                fs.statSync(CONTENT_PATH + footerPath);
+            } catch (error) {
+                errors.push('footer');
+            }
+        }
+
+        if (errors.length) {
+            callback(null, errors);
+            return;
+        }
+
+        callback(null);
+    });
+}
+
+LANGUAGES.forEach(language => {
+
+    describe(`content front matter (${language.toUpperCase()})`, function () {
+
+        var mdFiles = [];
+
+        before(function (done) {
+            var globOptions = {
+                cwd: CONTENT_PATH + language
+            };
+            mdFiles = glob.sync('**/*.md', globOptions);
+            done();
+        });
+
+        describe(`YAML syntax`, function () {
 
             it('has no syntax errors', async function () {
 
-                const filterGoodFiles = async (arr, predicate) => {
+                const filterOutGoodFiles = async (arr, predicate) => {
                     const results = await Promise.all(arr.map(predicate));
                     return arr.filter((_v, index) => results[index]);
                 }
 
-                const badFiles = await filterGoodFiles(mdFiles, async mdFile => {
-                    return await util.promisify(validateFrontMatterSyntax)(`${CONTENT_PATH}${language}/${mdFile}`);
+                const badFiles = await filterOutGoodFiles(mdFiles, async mdFile => {
+                    return await util.promisify(filterOutFilesWithValidFrontMatterSyntax)(`${CONTENT_PATH}${language}/${mdFile}`);
                 });
 
                 assert.strictEqual(badFiles.length, 0, `Found YAML syntax errors in the front matter of the following files:\n  ${badFiles.join('\n  ')}`);
+            });
+        });
+
+        describe('page fragments', function () {
+
+            const findFragmentProblems = async (arr) =>
+                await Promise.all(arr.map(mdFile => util.promisify(findProblemsWithFragments)(`${CONTENT_PATH}${language}/${mdFile}`)));
+
+            it('have valid paths', async function () {
+
+                const problems = await findFragmentProblems(mdFiles);
+                const badFiles = mdFiles
+                    .map((file, index) => problems[index] ? `${file} (${problems[index].join(', ')})` : undefined)
+                    .filter(mdFile => !!mdFile);
+
+                assert.strictEqual(badFiles.length, 0, `Found broken fragment paths in the front matter of the following files:\n  ${badFiles.join('\n  ')}`);
             });
         });
     });
